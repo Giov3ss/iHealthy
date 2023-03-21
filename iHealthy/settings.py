5e0dce810a46c0d0d57518496125a759621ac54d
Giov3ss <giovani.sp25@gmail.com>
2023-03-21 22:33:33 +0000

    importing pathlib and setting templates_dir

--- iHealthy/settings.py
+++ iHealthy/settings.py
@@ -14,11 +14,13 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 import django_heroku
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
