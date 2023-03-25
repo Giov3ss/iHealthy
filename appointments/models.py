73ce9d278b9a885bbbf3f5b0debf38699be2eb80
Giov3ss <giovani.sp25@gmail.com>
2023-03-25 21:52:03 +0000

    adding the class reason, so the user can choose

--- appointments/models.py
+++ appointments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Appointment(models.Model):
@@ -7,7 +8,18 @@ class Appointment(models.Model):
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=255)
-    reason = models.CharField(max_length=255)
+
+    class Reason(models.TextChoices):
+        weight_loss = 'WL', _('Weight Loss')
+        weight_gain = 'WG', _('Weight Gain')
+        better_healthy = 'BH', _('Better Healthy')
+        other = 'OT', _('Other')
+
+    reason = models.CharField(
+        max_length=2,
+        choices=Reason.choices,
+        default=Reason.weight_loss,
+    )
 
     def __str__(self):
         return f'{self.user.name} - {self.date} - {self.time}'
