167177953469f128992a107f3a8f60af23d9408b
Giov3ss <giovani.sp25@gmail.com>
2023-04-26 19:39:51 +0000

    adding handler403, 404, 500 to my views

--- appointments/views/appointments.py
+++ appointments/views/appointments.py
@@ -62,3 +62,14 @@ def delete(request, pk):
     
     return render(request, 'appointments/appointment_delete.html')
 
+
+def handler403(request, exception):
+    return render(request, 'errors/403.html', status=403)
+
+
+def handler404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500(request, exception):
+    return render(request, 'errors/500.html', status=500)
