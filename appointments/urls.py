3fb91b56b1c6957d2a69e0f60071f967355edc32
Giov3ss <giovani.sp25@gmail.com>
2023-04-26 20:02:12 +0000

    adding urls error 403, 404, 500

--- appointments/urls.py
+++ appointments/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('create/', appointments.create, name='appointment_create'),
     path('update/<int:pk>', appointments.update, name='appointment_update'),
     path('delete/<int:pk>', appointments.delete, name='appointment_delete'),
+    path('403', appointments.Page403.as_view(), name='403'),
+    path('404', appointments.Page404.as_view(), name='404'),
+    path('500', appointments.Page500.as_view(), name='500'),
 ]
+
--- appointments/views/appointments.py
+++ appointments/views/appointments.py
@@ -3,6 +3,7 @@ from ..models import Appointment
 from ..forms import AppointmentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.generic.base import TemplateView
 
 
 @login_required
@@ -63,13 +64,13 @@ def delete(request, pk):
     return render(request, 'appointments/appointment_delete.html')
 
 
-def handler403(request, exception):
-    return render(request, 'errors/403.html', status=403)
+class Page403(TemplateView):
+    template_name = 'errors/403.html'
 
 
-def handler404(request, exception):
-    return render(request, 'errors/404.html', status=404)
+class Page404(TemplateView):
+    template_name = 'errors/404.html'
 
 
-def handler500(request, exception):
-    return render(request, 'errors/500.html', status=500)
+class Page500(TemplateView):
+    template_name = 'errors/500.html'
