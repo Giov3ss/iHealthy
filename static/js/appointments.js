a2a347f4299aaa969580f0a1762a32ab4fc5df30
Giov3ss <giovani.sp25@gmail.com>
2023-05-18 23:03:33 +0000

    fixing timepicker js code

--- static/js/appointments.js
+++ static/js/appointments.js
@@ -1,10 +1,9 @@
 // make date time picker to start at 10am to 6pm 
 $(document).ready(function () {
-    (document.getElementById("id_time"));
     $('.timepicker').timepicker({
         timeFormat: 'HH:mm',
         interval: 60,
-        minTime: '10',
+        minTime: '10:00',
         maxTime: '18:00',
         defaultTime: '10:00',
         startTime: '10:00',
