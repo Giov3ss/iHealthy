9813ecddbc36bde1846cd85a4f6243db811bd114
Giov3ss <giovani.sp25@gmail.com>
2023-05-25 20:37:48 +0000

    Adding defects and defects of notes to my readme file

--- iHealthy/settings.py
+++ iHealthy/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 DEBUG = False
 if os.path.isfile('env.py'):
     import env
-    DEBUG = True
+    DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
